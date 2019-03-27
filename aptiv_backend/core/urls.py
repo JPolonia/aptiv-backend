69ae98c40d19e073d68ee1f1de602b8dc1201b40
diogovalentepcs <diogovalentepcs@gmail.com>
2019-03-27 05:12:11 +0000

    Frontend Fix

--- aptiv_backend/core/urls.py
+++ aptiv_backend/core/urls.py
@@ -6,4 +6,5 @@ from aptiv_backend.core.views import aptiv_validate
 urlpatterns = [
     url(r'^$', lambda r: HttpResponseRedirect('aptiv-validate')), # Redirects home page to core app
     url(r'^aptiv-validate$', aptiv_validate, name='aptiv_validate'),
+
 ]
